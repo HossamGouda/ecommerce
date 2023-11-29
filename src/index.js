c2487b768a4b25e03df174e90691e85a234f5ebb
Hossam Gouda <hos.gouda90@gmail.com>
2023-11-29 09:03:39 +0300

    adding other pages and Sass and finalizing the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // import "bootstrap/dist/js/bootstrap.min.js";   error for tooltip
 window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/scss/bootstrap.scss'
 import "./css/style.css";
 import "@fortawesome/fontawesome-free/js/all";
+import './sass/style.scss'
 
 document
   .querySelectorAll('[data-bs-toggle="tooltip"]')
@@ -40,6 +42,101 @@ document
         i.classList.remove("active");
       });
       item.parentNode.parentNode.classList.add("active");
+    });
+  });
+
+// حساب سعر أجمالي المنتج
+
+document.querySelectorAll("[data-product-quantity]").forEach((item) => {
+  item.addEventListener("change", () => {
+    const newQuantity = item.value;
+    const parent = item.closest("[data-product-info]");
+    const pricePerUnit = parent.getAttribute("data-product-price");
+    const totalPriceForProduct = newQuantity * pricePerUnit;
+    parent.querySelector(".total-price-for-product").innerHTML =
+      totalPriceForProduct + "$";
+
+    calculateTotalPrice();
+  });
+});
+
+document.querySelectorAll("[data-remove-from-card]").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.closest("[data-product-info]").remove();
+    calculateTotalPrice();
+  });
+});
+
+function calculateTotalPrice() {
+  let totalPriceForAllProduct = 0;
+  document.querySelectorAll("[data-product-info]").forEach((product) => {
+    const pricePerUnite = product.getAttribute("data-product-price");
+    const quantity = product.querySelector("[data-product-quantity]").value;
+    const totalPriceForProduct = pricePerUnite * quantity;
+
+    totalPriceForAllProduct = totalPriceForAllProduct + totalPriceForProduct;
+  });
+  document.getElementById("total-price-for-all-product").innerHTML =
+    totalPriceForAllProduct + "$";
+}
+const citiesByCountry = {
+  sa: ["جدة", "الرياض"],
+  eg: ["القاهرة", "الإسكندرية"],
+  jo: ["عمان", "الزرقاء"],
+  sy: ["دمشق", "حلب", "حماه"],
+};
+
+document.querySelectorAll('select[name="country"]').forEach((item) => {
+  item.addEventListener("change", () => {
+    const country = item.value;
+
+    const cities = citiesByCountry[country];
+
+    document
+      .querySelectorAll("#paymentcities option")
+      .forEach((option) => option.remove());
+
+    const firstOption = document.createElement("option");
+    const optionText = document.createTextNode("اختر المدينة");
+    firstOption.appendChild(optionText);
+    firstOption.setAttribute("value", "");
+    firstOption.setAttribute("disabled", "true");
+    firstOption.setAttribute("selected", "true");
+
+    const city_options = document.getElementById("paymentcities");
+    city_options.appendChild(firstOption);
+
+    cities.forEach((city) => {
+      const newOption = document.createElement("option");
+      const optionText = document.createTextNode(city);
+      newOption.appendChild(optionText);
+      newOption.setAttribute("value", city);
+      city_options.appendChild(newOption);
+    });
+  });
+});
+
+// أخفاء وإظهار حقول ادخال البطاقة الإئتمانية
+
+document
+  .querySelectorAll('#form-checkout input[name="payment-method"]')
+  .forEach((item) => {
+    item.addEventListener("change", () => {
+      const paymentMethod = item.value;
+
+      const creditCardInputs = document.querySelectorAll(
+        "#credit_card_info input"
+      );
+
+      if (paymentMethod === "on_delivery") {
+        creditCardInputs.forEach((input) => {
+          input.style.display = "none";
+        });
+      } else {
+        creditCardInputs.forEach((input) => {
+          input.style.display = "block";
+        });
+      }
     });
   });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, 'public'),
     },
     hot: false,
     port: 9000,
     open: true,
     devMiddleware: {
       writeToDisk: true,
-    },
+    }
   },
   module: {
     rules: [
@@ -33,20 +33,21 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
-        exclude: /bootstrap\.min\.css$/i,
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /custom\.scss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
               esModule: false,
             },
-          },
-          "css-loader",
-        ],
+          }
+           , 
+           "css-loader",
+          "sass-loader"],
       },
       {
-        test: /bootstrap\.min\.css$/i,
+        test:  /custom\.scss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
@@ -54,34 +55,54 @@ module.exports = {
               esModule: false,
             },
           },
-          "rtlcss-loader",
-        ],
+          'rtlcss-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-        generator: {filename: "./images/[name][ext]"},
+        type: 'asset/resource',
+        generator: {
+          filename: "./images/[name][ext]"
+        }
       },
       {
         test: /\.(svg|eot|woff|woff2|ttf)$/i,
-        type: "asset/resource",
-        generator: {filename: "./fonts/[name][ext]"},
+        type: 'asset/resource',
+        generator: {
+          filename: "./fonts/[name][ext]"
+        }
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "./src/index.html",
+      template: "./src/index.html"
     }),
     new HtmlWebpackPlugin({
       filename: "product.html",
-      template: "./src/product.html",
+      template: "./src/product.html"
     }),
-
+    new HtmlWebpackPlugin({
+      filename: "payment.html",
+      template: "./src/payment.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "checkout.html",
+      template: "./src/checkout.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "search.html",
+      template: "./src/search.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "./src/contact.html"
+    }),
     new MiniCssExtractPlugin({
-      filename: "css/style.css",
+      filename: "css/style.css"
     }),
-    new CssMinimizerPlugin(),
-  ],
+    new CssMinimizerPlugin()
+  ]
 };
